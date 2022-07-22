7da0199eaeaf7338ee3d55f7f710902b1d3d5cef
Hafeez Shaikh <hafeezshaikh197@gmail.com>
2022-07-22 20:34:01 +0500

    initial

---
+++ src/hashConnect.js
@@ -0,0 +1,27 @@
+import {HashConnect} from "hashconnect"
+
+const connectToWallet = async () => {
+    let hashconnect= new HashConnect()
+
+    let appData = {
+        name: "dApp Example",
+        description: "An example hedera dApp",
+        icon: "https://www.hashpack.app/img/logo.svg"
+    }
+
+    let initData = await hashconnect.init(appData)
+    let privateKey = initData.privKey
+
+    console.log("PrivateKey : " + privateKey)
+
+    let state = await hashconnect.connect()
+    let topic = state.topic
+
+    let pairingString = hashconnect.generatePairingString(state, 'mainnet', false)
+
+    hashconnect.findLocalWallets()
+
+    hashconnect.connectToLocalWallet(pairingString)
+
+}
+export default connectToWallet
