03a5c1141c096c832e8e57e969e2366584a10fd5
Hafeez Shaikh <hafeezshaikh197@gmail.com>
2022-07-23 00:43:22 +0500

    not

--- src/hashConnect.js
+++ src/hashConnect.js
@@ -12,18 +12,12 @@ const connectToWallet = async () => {
     let initData = await hashconnect.init(appData)
     let privateKey = initData.privKey
 
-    console.log("PrivateKey : " + privateKey)
-
     let state = await hashconnect.connect()
     let topic = state.topic
 
     let pairingString = hashconnect.generatePairingString(state, 'mainnet', false)
 
     hashconnect.findLocalWallets()
-    if(hashconnect.findLocalWallets() === undefined)
-    {
-        alert("Please Install HashPack Wallet ")
-    }
 
     hashconnect.connectToLocalWallet(pairingString)
 
